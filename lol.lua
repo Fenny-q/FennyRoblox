1e688c3e72ccc331cbf5c4906e5886fb22a8708f
Fenny <davies.liam.2009@gmail.com>
2025-03-25 18:21:31 +0100

    lol.lua

--- lol.lua
+++ lol.lua
@@ -1,1 +1,16 @@
-print("lol")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Fenny", "DarkTheme")
+
+------------------------------------------------------
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Made by Fenny", -- Required
+	Text = "fenny_q", -- Required
+	Icon = "rbxassetid://96767567864000" -- Optional
+})
+
+------------------------------------------------------
+
+-- Main
+local Home = Window:NewTab("- Offline -")
+local Section = Home:NewSection("Fenny doesnt work right now :( wait for the next Update")
